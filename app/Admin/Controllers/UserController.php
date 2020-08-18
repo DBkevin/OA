f4d1d677a45e2d66f65eb32ed16cb3adb62f58c9
Lee <416606903@outlook.com>
2020-08-18 08:59:40 +0000

    后台添加修改用户

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -7,6 +7,7 @@ use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends AdminController
 {
@@ -26,17 +27,27 @@ class UserController extends AdminController
     {
         $grid = new Grid(new User());
 
-        $grid->column('id', __('Id'));
-        $grid->column('name', __('Name'));
+        $grid->column('name',"名称");
+        $grid->column('phone','手机号码');
         return $grid;
     }
 
-    /**
+     /**
      * Make a show builder.
      *
-     * @param mixed $id
+     * @param mixed   $id
      * @return Show
      */
+    protected function detail($id)
+    {
+        $show = new Show(User::findOrFail($id));
+
+        $show->field('id', __('ID'));
+        $show->field("name","姓名");
+        $show->field("phone",'电话');
+
+        return $show;
+    }
 
     /**
      * Make a form builder.
@@ -47,8 +58,24 @@ class UserController extends AdminController
     {
         $form = new Form(new User());
 
-        $form->text('name', "名字");
-        $form->password('password', "输入密码");
+        $form->text('name', "名字")->rules("required");
+        $form->mobile('phone','电话号码/登陆名')->rules(function($form){
+            if(!$id=$form->model()->id){
+                return 'unique:users,min:11';
+            }
+        });
+        $form->password('password', "输入密码")->rules(function($form){
+            if(!$id=$form->model()->id){
+                return 'required,min:8,max:20';
+            }
+            return 'min:8';
+        });
+        $form->saving(function(Form $form){
+            $form->password=Hash::make($form->password);
+        });
+        $form->saved(function(Form $form){
+            return redirect('/admin/users');
+        });
         return $form;
     }
 }
